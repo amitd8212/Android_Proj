d62283c13afdc4a8150810267abe1d7897b98fa1
Amit Doshi <amitdoshirr@gmail.com>
2017-02-01 19:24:37 -0800

    Automation Practice

--- Ruby/February/lib/loginpage.rb
+++ Ruby/February/lib/loginpage.rb
@@ -5,13 +5,17 @@ require 'spec_helper'
 class LoginPage
 	include PageObject
 
-		def login(email_val, password_val)
+		def login(email_value, password_value)
 			email = @browser.text_field(id: 'user_email')
     	password = @browser.text_field(id: 'user_password')
     	submit = @browser.button(id: 'submit_button')
-    	email.set email_val
-    	password.set password_val
+    	email.set email_value
+    	password.set password_value
     	submit.click
+		end
+
+		def logout
+
 		end
   	
 
