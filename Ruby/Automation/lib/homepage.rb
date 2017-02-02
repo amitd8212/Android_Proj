14060ba9e193e2c47704629239dc5fea8b70e87b
Amit Doshi <amitdoshirr@gmail.com>
2017-02-02 13:37:28 -0800

    First task complete

--- Ruby/Automation/lib/homepage.rb
+++ Ruby/Automation/lib/homepage.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
+HOME_PAGE_URL = "https://app.recurly.com/login?"
+
+#This Page will hold all objects on the Home Page.
 
 
 class HomePage
 	include PageObject
+
+
+	def go_to_account_page
+		#Click on Account page link from side bar menu
+		account_link = @browser.link(text: 'Accounts')
+		account_link.click
+	end
+
+
 
 end
---
+++ Ruby/Automation/lib/urls.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+ 
+
+#This Page will hold all site urls in a hash.
+
+
+class UrlValues
+	include PageObject
+
+	def url_loc(username, pt)
+
+		@@pages = Hash[ "HOME" => "https://app.recurly.com/login?",
+					"ACCOUNT" => "https://#{username}.recurly.com/accounts"]
+
+		return @@pages[pt]
+
+	end
+
+
+end
