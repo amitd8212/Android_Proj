56f1f3af969b8df4c1ffe0638ef531d04c92a33d
Amit Doshi <amitdoshirr@gmail.com>
2017-02-05 23:20:43 -0800

    Final checkin for Api work

--- Ruby/Recurly/lib/apipage.rb
+++ Ruby/Recurly/lib/apipage.rb
@@ -13,18 +13,26 @@ require 'nokogiri'
 class ApiPage
 	include PageObject
 
-	def create_account(account_file)		
-		@cac = File.open(account_file) {|f| Nokogiri::XML(f) }
-		@new_acount_code = @cac.css('//account_code').last.text
-		@cac.close
-		return @new_account_code
+	def create_account(account_file)
+		#Return account code from last created account.	
+		@xml_doc_1 = Nokogiri::HTML(account_file)	
+		return @xml_doc_1.css('//account_code').first.text
 	end
 
 
-	def get_accounts(input, xml_doc)
-		@account_code_value = xml_doc.css('//account_code').last.text
-		puts xml_doc.css('//account').last
-		return @account_code_value
+	def get_accounts(input_file)
+		#Return account code from xml file.
+		@xml_doc = Nokogiri::HTML(input_file)
+		return @xml_doc.css('//account_code').first.text
+	end
+
+	def validate_accounts(created_account_code, expected_account_code)
+		assert created_account_code == expected_account_code
+		if created_account_code == expected_account_code
+			puts "Account created correctly"
+		else
+			puts "Account was not created correctly"
+		end
 	end
 
 end
